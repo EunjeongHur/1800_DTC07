6a6fb44e3e2d268a10724f6f54c16574ef3b8670
EunjeongHur <ungdunge2@gmail.com>
2022-10-27 10:18:25 -0700

    add script that loads navbar and footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-function sayHello() {
-    
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
-//sayHello();
+loadSkeleton();  //invoke the function
