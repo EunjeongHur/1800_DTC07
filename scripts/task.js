5985231241d39fe52cb5d5c8d077c8a87f09488c
EunjeongHur <ungdunge2@gmail.com>
2022-11-06 18:16:42 -0800

    Add searchbar

--- scripts/task.js
+++ scripts/task.js
@@ -4,6 +4,7 @@ function displayTask(collection) {
     db.collection(collection).get()
         .then(snap => {
             snap.forEach(doc => {
+                var docid = doc.id;
                 var title = doc.data().title;
                 var type = doc.data().type;
                 var date = doc.data().date;
@@ -13,11 +14,18 @@ function displayTask(collection) {
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-date').innerHTML = `Due ${date}`
                 newcard.querySelector('.card-text').innerHTML = description;
+                newcard.querySelector('.card-id').innerHTML = docid;
 
                 document.getElementById(collection + "-go-here").appendChild(newcard);
             })
         })
 }
+
+$('#search').keyup(function (){
+    $('.card').removeClass('d-none');
+    var card_title = $(this).val();
+    $('.card-deck').find('.card .card-body h5:not(:contains("'+card_title+'"))').parent().parent().addClass('d-none');
+})
 
 firebase.auth().onAuthStateChanged((user) => {
     // Check if a user is signed in:
