180eb9d2b82f5549dd9b5f00b169c5f92ce9d692
EunjeongHur <ungdunge2@gmail.com>
2022-11-14 20:59:47 -0800

    firebase storage

--- scripts/submission.js
+++ scripts/submission.js
@@ -36,9 +36,101 @@ function viewDetails(id) {
         })
 }
 
+// function uploadFileHandler(){
+    
+//     firebase.auth().onAuthStateChanged(function(user){
+//         if (user){
+//             var fileInput = document.getElementById("formFileMultiple");
+//             var image = document.getElementById("image-goes-here")
+//             var file = fileInput.files[0];
+//             var blob = URL.createObjectURL(file);
+
+//             // image.src = blob
+//             // console.log(file)
+//             // console.log(blob);
+//             const pickedfile = fileInput.files[0];  // file that user picked
+//             image.src = URL.createObjectURL(pickedfile)
+
+            
+//             // var pickedfile = file
+//             console.log(file)
+
+//             console.log(file.name)
+//             let demo = {
+//                 "name": `${file.name}`,
+//                 "lastModified": `${file.lastModified}`,
+//                 "size": `${file.size}`,
+//                 "type": `${file.type}`
+//             }
+
+//             console.log(demo);
+//             var storageRef = firebase.storage().ref("images/" + user.uid + ".png");
+//             storageRef.put(demo)
+//                 .then(function(){
+// 	                storageRef.getDownloadURL()
+//                         .then(function (url) { // Get URL of the uploaded file
+//                             console.log(url); // Save the URL into users collection
+//                             db.collection("users").doc(user.uid).update({
+//                                 "profile-pic": url
+//                             })
+//                         })
+//                 })
+            
+//             // storageRef.put(file)
+//             //     .then(function(){
+//             //         console.log("hhh")
+//             //         console.log('Uploaded to Cloud Storage');
+//             //     })
+//             //     .catch(function(err){
+//             //         console.log(err)
+//             //         console.log("error occurs:" + err);
+//             //     })
+//             }
+//         }) 
+// }
+                
+            // storageRef.getDownloadURL()
+            //     .then(function(url){
+            //         console.log(url);
+            //         db.collection("users").doc(user.uid).update({
+            //             "submission": url
+            //         })
+            //         .then(function(){
+            //             console.log('Added url to Firestore.');
+            //         })
+            //     })
+
+            // fileInput.addEventListener('change', function(e) {
+                
+            //     var file = e.target.files[0];
+            //     var storageRef = storage.ref(user.uid + ".pdf");
+            //     console.log("hhh")
+            //     console.log(storageRef);
+            //     console.log("hhh")
+            //     storageRef.put(file)
+            //         .then(function(){
+            //             console.log('Uploaded to Cloud Storage');
+            //         })
+                
+            //     storageRef.getDownloadURL()
+            //         .then(function(url){
+            //             console.log(url);
+            //             db.collection("users").doc(user.uid).update({
+            //                 "submission": url
+            //             })
+            //             .then(function(){
+            //                 console.log('Added url to Firestore.');
+            //             })
+            //         })
+            //     }
+            // )
+            // window.location.href="task.html"
+
+
 function setup(){
     let taskID = localStorage.getItem("taskID");
     viewDetails(taskID)
+    $("#uploadFile").click(uploadFileHandler);
 }
 
 $(document).ready(setup);
