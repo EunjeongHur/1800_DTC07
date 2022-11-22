a63ccfb882bc6b1d64a05418f3df79470ea9f54d
EunjeongHur <ungdunge2@gmail.com>
2022-11-21 19:13:34 -0800

    add a feature that student can check if the specific task is done

--- scripts/submission.js
+++ scripts/submission.js
@@ -1,3 +1,5 @@
+var currentUser;
+
 function viewDetails(id) {
     // Using URL 
         // const url = window.location.search;
@@ -39,144 +41,36 @@ function viewDetails(id) {
         })
 }
 
-
-
-// function uploadFileHandler(){
-    
-//     firebase.auth().onAuthStateChanged(function(user){
-//         if (user){
-//             var fileInput = document.getElementById("formFileMultiple");
-//             fileInput.addEventListener('change', function(e) {
-//                 var file = e.target.files[0];
-//                 console.log(file)
-//                 var storageRef = firebase.storage().ref("submissions/" + user.uid + ".pdf");
-            
-//                 storageRef.put(file)
-//                     .then(function(){
-// 	                    storageRef.getDownloadURL()
-//                             .then(function (url) { // Get URL of the uploaded file
-//                                 console.log(url); // Save the URL into users collection
-//                                 db.collection("users").doc(user.uid).update({
-//                                 "profile-pic": url
-//                             })
-//                         })
-//                     })
-//             })
-//             // var file = fileInput.files[0];
-//             // console.log(file)
-//             // console.log(file.name)
-
-//             // // let demo = {
-//             // //     "name": `${file.name}`,
-//             // //     "lastModified": `${file.lastModified}`,
-//             // //     "size": `${file.size}`,
-//             // //     "type": `${file.type}`
-//             // // }
-
-//             // // console.log(demo);
-//             // var storageRef = firebase.storage().ref("submissions/" + user.uid + ".pdf");
-//             // storageRef.put(file)
-//             //     .then(function(){
-// 	        //         storageRef.getDownloadURL()
-//             //             .then(function (url) { // Get URL of the uploaded file
-//             //                 console.log(url); // Save the URL into users collection
-//             //                 db.collection("users").doc(user.uid).update({
-//             //                     "profile-pic": url
-//             //                 })
-//             //             })
-//             //     })
-
-            
-//             // storageRef.put(file)
-//             //     .then(function(){
-//             //         console.log("hhh")
-//             //         console.log('Uploaded to Cloud Storage');
-//             //     })
-//             //     .catch(function(err){
-//             //         console.log(err)
-//             //         console.log("error occurs:" + err);
-//             //     })
-//             }
-//         }) 
-// }
-                
-            // storageRef.getDownloadURL()
-            //     .then(function(url){
-            //         console.log(url);
-            //         db.collection("users").doc(user.uid).update({
-            //             "submission": url
-            //         })
-            //         .then(function(){
-            //             console.log('Added url to Firestore.');
-            //         })
-            //     })
-
-            // fileInput.addEventListener('change', function(e) {
-                
-            //     var file = e.target.files[0];
-            //     var storageRef = storage.ref(user.uid + ".pdf");
-            //     console.log("hhh")
-            //     console.log(storageRef);
-            //     console.log("hhh")
-            //     storageRef.put(file)
-            //         .then(function(){
-            //             console.log('Uploaded to Cloud Storage');
-            //         })
-                
-            //     storageRef.getDownloadURL()
-            //         .then(function(url){
-            //             console.log(url);
-            //             db.collection("users").doc(user.uid).update({
-            //                 "submission": url
-            //             })
-            //             .then(function(){
-            //                 console.log('Added url to Firestore.');
-            //             })
-            //         })
-            //     }
-            // )
-            // window.location.href="task.html"
-
-
-var TaskFile;
-
-function chooseFileListener(){
-    const fileInput = document.getElementById("submit-file-input");
-    console.log(fileInput)
-
-    fileInput.addEventListener('change', function(e){
-        TaskFile = e.target.files[0];
-        var blob = URL.createObjectURL(TaskFile)
+function DoneTaskHandler(taskID){
+    console.log("button clicked")
+    currentUser.set({
+        done_tasks: firebase.firestore.FieldValue.arrayUnion(taskID)
+    },{
+            merge: true
     })
-    console.log(TaskFile)
+    .then(function(){
+        console.log("This task is done for: " + currentUser )
+        // var 
+    })
+    window.location.href="task.html";
 }
 
-chooseFileListener()
-
-
-function submitTaskFile() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        // var storageRef = storage.ref("works/" + user.uid + ".jpg");
-        var storageRef = storage.ref("works/" + user.uid + ".pdf");
-        console.log(TaskFile)
-        storageRef.put(TaskFile)
-            .then(function() {
-                console.log("Uploaded to Cloud Storage.");
-
-                storageRef.getDownloadURL()
-                    .then(function (url) {
-                        console.log("Got the download URL.");
-                        console.log(url);
-                        // db.collection("submissions").doc(
-                    })
-            })
-    }
-)}
 
 function setup(){
-    let taskID = localStorage.getItem("taskID");
-    viewDetails(taskID)
-    // $("#uploadFile").click(uploadFileHandler);
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid);
+            let taskID = localStorage.getItem("taskID");
+            viewDetails(taskID)
+            $(".submission_submit_btn").attr("doneTask", taskID)
+            $("body").on('click', '.submission_submit_btn', function(){
+                DoneTaskHandler(taskID)
+            })
+           
+        } else {
+            // do something else
+        }
+    })
 }
 
 $(document).ready(setup);
