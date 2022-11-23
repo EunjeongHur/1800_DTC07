86ed439fa176db246e2f3a82fe754b494b9f7b73
EunjeongHur <ungdunge2@gmail.com>
2022-11-23 11:06:48 -0800

    instructor can see all posted tasks

--- scripts/task.js
+++ scripts/task.js
@@ -20,6 +20,82 @@ function getuserSet(uid) {
 		});
 }
 
+
+function displayTaskforInstructor(uid) {
+	var current_location = $(location).attr("href");
+	if (current_location.endsWith("task.html")) {
+		let cardTemplate = document.getElementById("taskCardTemplate");
+		// Calculate how many days left
+		let today_date = new Date();
+		let year = today_date.getFullYear();
+		let month = String(today_date.getMonth() + 1).padStart(2, "0");
+		let date = String(today_date.getDate()).padStart(2, "0");
+		var newdate = year + month + date;
+		
+		db.collection("tasks")
+			.orderBy("date")
+			.get()
+			.then((snap) => {
+				var counter = 0;
+				snap.forEach((doc) => {
+					var docid = doc.id;
+					
+					var course_num = doc.data().course_num;
+					var title = doc.data().title;
+					var type = doc.data().type;
+					var date = doc.data().date;
+					var description = doc.data().description;
+					let newcard = cardTemplate.content.cloneNode(true);
+
+					let formatted_task_date = date.replaceAll("-", "");
+					var time_left =
+						Number(formatted_task_date) - Number(newdate);
+
+					if (time_left < -1) {
+						// do something
+					} else {
+						newcard.querySelector(".card-title").innerHTML =
+							title;
+						newcard.querySelector(
+							".card-course-num"
+						).innerHTML = `${course_num} -&nbsp;`;
+						newcard.querySelector(".card-type").innerHTML =
+							type;
+						let only_date = date.replaceAll("-", "/").slice(5);
+						let translated_date = getMonthName(only_date);
+						newcard.querySelector(
+							".card-onlydate"
+						).innerHTML = `&nbsp;(${translated_date})`;
+						newcard.querySelector(
+							".card-date"
+						).innerHTML = `Due ${date}`;
+						newcard.querySelector(".card-text").innerHTML =
+							description;
+						newcard
+							.querySelector(".card-id")
+							.setAttribute("docid", docid);
+						newcard.querySelector(
+							"#collapseCard"
+						).id = `collapseCard${counter}`;
+						newcard
+							.querySelector("#collapseCardtarget")
+							.setAttribute(
+								"data-bs-target",
+								`#collapseCard${counter}`
+							);
+						document
+							.getElementById("tasks" + "-go-here")
+							.appendChild(newcard);
+						counter += 1;
+					}
+			});
+		});
+
+	};
+}
+
+
+
 function displayTask(uid) {
 	var current_location = $(location).attr("href");
 	if (current_location.endsWith("task.html")) {
@@ -134,7 +210,18 @@ firebase.auth().onAuthStateChanged((user) => {
 		currentUser = db.collection("users").doc(user.uid);
 		var current_uid = user.uid;
 		$("#name-goes-here").text(user.displayName);
-		displayTask(current_uid);
+		db.collection("users").doc(user.uid).get()
+			.then(userDoc => {
+				var current_user_type = userDoc.data().type;
+				if (current_user_type == 'Student'){
+					// console.log("student")
+					displayTask(current_uid);
+				} else {
+					displayTaskforInstructor(current_uid);
+					$('h1').html('Posted Tasks')
+				}
+			})
+		
 		$("#success-alert")
 			.fadeTo(2000, 500)
 			.slideUp(500, function () {
