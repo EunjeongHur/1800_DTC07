07b2f1520e5f2896ab1e1aeb44d4d9f66378d554
EunjeongHur <ungdunge2@gmail.com>
2022-11-22 18:39:05 -0800

    change navbar

--- scripts/log_in.js
+++ scripts/log_in.js
@@ -2,6 +2,20 @@ firebase.auth().onAuthStateChanged((user) => {
     // Check if a user is signed in:
     if (user) {
         console.log("User logged in.")
+        db.collection("users").doc(user.uid).get()
+            .then(userDoc => {
+                var current_user_type = userDoc.data().type;
+                if (current_user_type == 'Student'){
+                    console.log("student")
+                    $('#nav_for_instructor').css('display', 'none');
+                } else {
+                    console.log("This user is an Instructor")
+                    $('#nav_for_instructor').css('display', 'block');
+                }
+            });
+        
+        // var current_user_type = currentUser.
+        // console.log(current_user_type)
     } else {
         document.getElementById("logged_in").style.display="none";
         window.location.href="index.html"
